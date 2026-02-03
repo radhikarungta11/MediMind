bb2c5f53055b2581a7431e67e7f433a161e77ed9
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2026-02-03 21:48:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request  
 from dotenv import load_dotenv
 import os
 from langchain_pinecone import PineconeVectorStore
