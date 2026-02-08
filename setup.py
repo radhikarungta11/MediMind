451d3dc01279e16e1dd132b5975cd9263b8eca96
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2026-02-08 13:47:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'Generative AI Project',
-    version= '0.0.0',
+    version= '0.0.0',  
     author= 'Bappy Ahmed',
     author_email= 'entbappy73@gmail.com',
     packages= find_packages(),   
