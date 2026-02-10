27b7c0396a7b10a56fd880e594732a55e7a406dc
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2026-02-10 11:35:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request  
 from dotenv import load_dotenv
 import os  
-from langchain_pinecone import PineconeVectorStore  
+from langchain_pinecone import PineconeVectorStore    
 from src.helper import download_hugging_face_embeddings   
 from src.prompt import system_prompt  
 from langchain_core.documents import Document  
